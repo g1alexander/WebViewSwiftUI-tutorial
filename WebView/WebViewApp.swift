1a71646026c8060330f27650583a8adae67b8a5c
g1alexander <alexlds26@gmail.com>
2022-11-07 14:33:28 -0500

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by g1alexander on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
